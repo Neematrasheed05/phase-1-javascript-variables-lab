91004672730edd0e89c7bc4328ccabf5b94cca8a
Neematrashid <nymatrasheed05@gmail.com>
2023-09-21 12:28:44 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //code your solution in thsi file!
 const companyName = 'Scuber';
 // let companyName = 'Scuber';
+//defined as const so as the value doesnt change
 
 let companyCeo = 'Susan Smith';
+//let allows room for change through the program
 let mostProfitableNeighborhood = 'Chelsea';
